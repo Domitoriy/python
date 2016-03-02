64c87b586874505ba719a1a05571ccc64720ff85
Domitoriy <dmitro1994.fisenko@yandex.ua>
2016-03-02 10:50:31 +0200

    add all

--- VagrantApp/myapp/myapp.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "<body style='background-color:black'> <FONT face='courier'> <h1 style='color:lightgreen'>Wake up, Neo...</h1> </FONT> </body>"
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0')
